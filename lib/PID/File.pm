af630a72b81a89334eae94bec2a4a281a6b1836d
Rob Brown <rob@intelcompute.com>
2012-06-01 23:35:52 +0100

    updated version

--- lib/PID/File.pm
+++ lib/PID/File.pm
@@ -16,11 +16,11 @@ PID::File - PID files that guard against exceptions.
 
 =head1 VERSION
 
-Version 0.13
+Version 0.14
 
 =cut
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
